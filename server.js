4d0c076bd89da039fb414d7cbbe4f165945b0628
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-09-19 13:36:25 +0100

    ch: connected to database

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
 
-// const port = process.env.PORT || 3000;
-const port = 3000;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection succesful!');
+  })
+  .catch((err) => console.error('MongoDB connection error: ', err));
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
