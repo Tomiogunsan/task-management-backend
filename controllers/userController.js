5e48427ef3ea1fe82f2ed30cd5fb1089c61727ce
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-09-24 14:04:31 +0100

    ch: implemented add task to members endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,5 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
 
 exports.getAllUser = catchAsync(async (req, res, next) => {
   const users = await User.find();
