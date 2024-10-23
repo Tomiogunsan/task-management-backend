a6177434a475a5b40806a992a4dfc25ed7447122
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-10-23 12:13:48 +0100

    fix: fixed getAllTask functionality

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -22,7 +22,9 @@ exports.createTask = catchAsync(async (req, res) => {
 exports.getAllTask = catchAsync(async (req, res, next) => {
   const tasks = await Task.find()
     .populate('project', '_id name')
+    .populate('assignedUser', 'name')
     .sort({ dateCreated: -1 });
+  
   if (!tasks) {
     return next(new AppError('No task found ', 404));
   }
